9c715298b0246b5b66a7b7d56baa21d339ba84cf
Boris Civcic <civcicboris93@gmail.com>
2018-08-19 15:48:57 +0200

    lodash removed

--- src/containers/showAlbums.js
+++ src/containers/showAlbums.js
@@ -3,25 +3,44 @@ import { connect } from 'react-redux'
 import { fetchAlbums } from '../actions/index'
 
 class ShowAlbums extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      albums: []
+    }
+  }
   componentDidMount() {
     this.props.fetchAlbums()
   }
 
-  renderAlbums() {
-    return _.map(this.props.albums, album => {
-      return (
-        <li className="list-group-item" key={album.id}>
-          {album.title}
-        </li>
-      )
-    })
-  }
+  // renderAlbums() {
+  //   return _.map(this.props.albums, album => {
+  //     return (
+  //       <li className="list-group-item" key={album.id}>
+  //         {album.title}
+  //       </li>
+  //     )
+  //   })
+  // }
 
   render() {
+    const {
+      albums
+    } = this.state;
     console.log(this.props.albums)
     return (
       <div>
-        <ul>{this.renderAlbums()}</ul>
+        <ul>
+        {
+          albums && albums.map((x, i) => {
+            return (
+              <li key={i} >
+                {i.name}
+              </li>
+              );
+            })
+          }
+        </ul>
       </div>
     )
   }
