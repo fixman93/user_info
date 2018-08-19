383d55c25ab2bae4b64b5438f71c8616ea980dbe
Boris Civcic <civcicboris93@gmail.com>
2018-08-19 19:24:25 +0200

    albums bug fixed

--- src/containers/showAlbums.js
+++ src/containers/showAlbums.js
@@ -1,46 +1,38 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchAlbums } from '../actions/index'
 
 class ShowAlbums extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      albums: []
-    }
-  }
   componentDidMount() {
     this.props.fetchAlbums()
   }
 
-  // renderAlbums() {
-  //   return _.map(this.props.albums, album => {
-  //     return (
-  //       <li className="list-group-item" key={album.id}>
-  //         {album.title}
-  //       </li>
-  //     )
-  //   })
-  // }
+  renderAlbums() {
+    return _.map(this.props.albums, album => {
+      return (
+        <li className="list-group-item" key={album.id}>
+          {album.id}. {album.title}
+        </li>
+      )
+    })
+  }
 
   render() {
-    const {
-      albums
-    } = this.state;
+    var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+    const result = words.filter(word => word.length > 6);
+
+    console.log(result);
+    // const filter = (...albums) => {
+    //   return albums.filter(el => el.id === 2)
+    // }
+    // console.log(filter(this.props.albums))
     console.log(this.props.albums)
     return (
       <div>
-        <ul>
-        {
-          albums && albums.map((x, i) => {
-            return (
-              <li key={i} >
-                {i.name}
-              </li>
-              );
-            })
-          }
-        </ul>
+        <h3>Albums</h3>
+        <ul>{this.renderAlbums()}</ul>
       </div>
     )
   }
