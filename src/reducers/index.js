a22190652003190a04b3836cf2e964541a1d00a3
Boris Civcic <civcicboris93@gmail.com>
2018-08-19 14:31:38 +0200

    show all albums

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import UsersReducer from './reducer_users'
+import AlbumsReducer from './reducer_albums'
 
 const rootReducer = combineReducers({
-  users: UsersReducer
+  users: UsersReducer,
+  albums: AlbumsReducer
 });
 
 export default rootReducer;
--- src/reducers/reducer_albums.js
+++ src/reducers/reducer_albums.js
@@ -1,9 +1,9 @@
 import _ from 'lodash'
-import { FETCH_USERS, FETCH_ALBUMS } from '../actions/index'
+import { FETCH_ALBUMS } from '../actions/index'
 
 export default function(state = {}, action) {
   switch (action.type) {
-    case FETCH_USERS:
+    case FETCH_ALBUMS:
       return _.mapKeys(action.payload.data, 'id')
     default:
      return state
